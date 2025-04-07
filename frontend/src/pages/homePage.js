22f56549f778dcfd003298abb3661f3b60786804
Srijan Sasmal <srijansasmal88@gmail.com>
2025-04-07 18:54:26 +0530

    Add Login and Signup components for user authentication - Utilized Chakra UI for styling and layout.

--- frontend/src/pages/homePage.js
+++ frontend/src/pages/homePage.js
@@ -1,11 +1,52 @@
-import React from 'react'
+import React from 'react';
+import {
+  Container,
+  Box,
+  Text,
+  Tab,
+  Tabs,
+  TabList,
+  TabPanel,
+  TabPanels,
+} from '@chakra-ui/react';
+import Login from '../components/authendication/Login';
+import Signup from '../components/authendication/Signup';
 
 const homePage = () => {
   return (
-    <div>
-      Home
-    </div>
-  )
-}
+    <Container maxW="xl" centerContent>
+      <Box
+        display="flex"
+        justifyContent="center"
+        p={3}
+        bg="white"
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize="4xl" fontFamily="Work sans" color="black">
+          HushTalk
+        </Text>
+      </Box>
+      <Box bg="white" w="100%" p={4} borderRadius="lg" color= 'black' borderWidth="1px">
+        <Tabs variant="soft-rounded" colorScheme="teal">
+          <TabList mb= '1em'>
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login/>
+            </TabPanel>
+            <TabPanel>
+              <Signup/>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
+};
 
-export default homePage
+export default homePage;
