25c45738db2b160602cad3a02c8da238f7c57173
Srijan Sasmal <srijansasmal88@gmail.com>
2025-04-08 20:55:27 +0530

    Refactor database connection logic and add constants file for database name

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import {app} from "./app.js";
+import connectDB from "./db/index.js";
 
 dotenv.config({
     path: './env'
@@ -7,6 +8,15 @@ dotenv.config({
 
 const port= (process.env.PORT || 5000)
 
-app.listen(port, ()=>{
-    console.log(`⚙️  Server is running at port: ${port}`)
+connectDB().then(()=>{
+    app.on("error", (error)=>{
+        console.log("ERROR", error);
+        throw error;
+    })
+
+    app.listen(port, ()=>{
+        console.log(`⚙️  Server is running at port: ${port}`)
+    })
+}).then().catch((error)=>{
+    console.log(`MONGO connection failed !!! `, err)
 })
